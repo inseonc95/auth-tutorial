f403e81e62378148db5d61bd3e3c5cb742a2ab64
kylie <inseonc@alphacode.co.kr>
2024-01-15 19:04:15 +0900

    feat: Add auth, auth route, middleware using next auth

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { auth } from "./auth"
+export default auth((req) => {
+  console.log("ROUTE:", req.nextUrl.pathname)
+  // req.auth
+})
+
+// Optionally, don't invoke Middleware on some paths
+// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+export const config = {
+  // matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  // matcher: ["/auth/login", "/auth/register"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+}
