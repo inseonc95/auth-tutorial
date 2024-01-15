7ccd174768f501b69cd18ea193620fe32e27fd05
kylie <inseonc@alphacode.co.kr>
2024-01-15 16:38:05 +0900

    feat: Separate db method to file

--- actions/register.ts
+++ actions/register.ts
@@ -5,7 +5,7 @@ import bcryp from "bcrypt";
 
 import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
-
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   // Validate the values
@@ -19,7 +19,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const { email, password, name } = validatedFields.data;
   const hashedPassword = await bcryp.hash(password, 10);
 
-  const existingUser = await db.user.findUnique({ where: { email } });
+  const existingUser = await getUserByEmail(email);
   console.log(existingUser);
   if (existingUser) {
     return { error: "Email already in use!" };
