394da5e03c40759e57b6952d0792377d29947f1a
kylie <inseonc@alphacode.co.kr>
2024-01-16 13:32:58 +0900

    feat: Add auth error page

---
+++ components/auth/error-card.tsx
@@ -0,0 +1,27 @@
+// ErrorCard
+
+import { Header }  from "@/components/auth/header";
+import { BackButton } from "@/components/auth/back-button";
+import {
+  Card,
+  CardFooter,
+  CardHeader
+} from "@/components/ui/card";
+
+export const ErrorCard = () => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label="Oops! Something went wrong!">
+        </Header>
+      </CardHeader>
+      <CardFooter>
+      <BackButton
+        label="Back to login"
+        href="/auth/login"
+      />
+      </CardFooter>
+    </Card>
+  )
+
+}
