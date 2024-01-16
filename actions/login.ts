cfa20952178ec5db647980bad0304d44bdfd1dc4
kylie <inseonc@alphacode.co.kr>
2024-01-16 15:52:47 +0900

    feat: Clean existing code

--- actions/login.ts
+++ actions/login.ts
@@ -25,17 +25,21 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
 
   const existingUser = await getUserByEmail(email);
 
-  if(!existingUser || !existingUser.email || !existingUser.password) {
-    return { error: "Email does not exits!" };
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: "Email does not exist!" }
   }
 
-  if(!existingUser.emailVerified) {
-    const verificationToken = await generateVerificationToken(existingUser.email);
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(
+      existingUser.email,
+    );
+
     await sendVerificationEmail(
       verificationToken.email,
       verificationToken.token,
     );
-    return { success : "Confirmation email sent!!!" };
+
+    return { success: "Confirmation email sent!" };
   }
 
   try {
