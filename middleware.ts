0d0f7ec577a96629d67e2dff30e99fbb11e3a95c
kylie <inseonc@alphacode.co.kr>
2024-01-16 10:19:08 +0900

    feat: Update middleware based on route url

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,37 @@
 import NextAuth from "next-auth"
 import authConfig from '@/auth.config'
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes"
 export const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-  console.log("ROUTE:", req.nextUrl.pathname);
-  console.log("IS LOGGEDIN:", isLoggedIn);
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) { // public 보다 이전에 위치해야함 왜냐하면 public이 authRoute에 포함되어 있기 때문에
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return null;
 })
 
 export const config = {
---
+++ routes.ts
@@ -0,0 +1,34 @@
+/**
+ * An array of routes that are accessible to the public.
+ * Thes routes do not require authentication.
+ * @type {string[]}
+ */
+
+export const publicRoutes = [
+  "/",
+  // "/settings", <- url: /settings에 접근가능하게 하려면 이렇게 해야함
+];
+
+/**
+ * An array of routes that are used for authentication.
+ * Thes routes will redirect logged in users to / settings
+ * @type {string[]}
+ */
+
+export const authRoutes = [
+  "/auth/login",
+  "/auth/register",
+]
+
+/**
+ * The prefix for API authentication routes.
+ * Routes that start with this prefix are used for API authentication purposes.
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after logging in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
