a89df21d408345f23dbd0b300a8a9e6a42bc8030
kylie <inseonc@alphacode.co.kr>
2024-01-16 13:55:14 +0900

    feat: Wrap error card with card rapper

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -1,27 +1,17 @@
-// ErrorCard
-
-import { Header }  from "@/components/auth/header";
-import { BackButton } from "@/components/auth/back-button";
-import {
-  Card,
-  CardFooter,
-  CardHeader
-} from "@/components/ui/card";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 
 export const ErrorCard = () => {
   return (
-    <Card className="w-[400px] shadow-md">
-      <CardHeader>
-        <Header label="Oops! Something went wrong!">
-        </Header>
-      </CardHeader>
-      <CardFooter>
-      <BackButton
-        label="Back to login"
-        href="/auth/login"
-      />
-      </CardFooter>
-    </Card>
+    <CardWrapper
+      headerLabel="Oops! Something went wrong!"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+      >
+        <div className="w-full flex justify-center items-center">
+          <ExclamationTriangleIcon className="text-destructive" />
+        </div>
+      </CardWrapper>
   )
 
 }
