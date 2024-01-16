be3028577dd897e86bd518b458cf33e2ef68a3ea
kylie <inseonc@alphacode.co.kr>
2024-01-16 11:26:55 +0900

    feat: Add session manufacture?

--- auth.ts
+++ auth.ts
@@ -4,6 +4,9 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "@/lib/db"
 import authConfig from '@/auth.config'
 
+import { getUserById } from "@/data/user"
+import { kMaxLength } from "buffer"
+
 export const {
   handlers: { GET, POST },
   auth,
@@ -12,12 +15,25 @@ export const {
 } = NextAuth({
   callbacks: {
     async session({ token, session }) {
+      console.log({sessionToken: token})
       if (token.sub && session.user) {
         session.user.id = token.sub;
-      }  
+      }
+
+      if(token.role && session.user) {
+        session.user.role = token.role;
+      }
       return session;
     },
     async jwt({ token })  {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token;
     }
   },
